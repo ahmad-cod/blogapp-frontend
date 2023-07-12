4ba56786d7785723dc1046f325546b62b9484c41
ahmad-cod <ahmadadewale800@gmail.com>
2023-07-12 02:02:14 +0100

    Fix the functionality

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,70 +1,142 @@
-import { useState } from "react"
-import authService from "../../services/auth"
-import blogService from "../../services/blogs"
-import Header from "../Header"
-import SignupForm from "./SignupForm"
-import Notification from "../Notification"
-import { useDispatch } from "react-redux"
-import { setNotification } from "../../reducers/notificationReducer"
-import { setUser } from "../../reducers/userReducer"
-import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+import authService from "../../services/auth";
+import blogService from "../../services/blogs";
+import Header from "../Header";
+import SignupForm from "./SignupForm";
+import Notification from "../Notification";
+import { useDispatch } from "react-redux";
+import { setNotification } from "../../reducers/notificationReducer";
+import { setUser } from "../../reducers/userReducer";
+import { Link, useNavigate } from "react-router-dom";
+import illustrationMockup from "../../assets/illustration-working.svg";
 
 const LoginForm = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(username, 'username', password, 'password')
 
     try {
-      const user = await authService.login({ username, password })
-      console.log(user)
+      const user = await authService.login({ username, password });
+      console.log(user);
       if (!user) {
-        dispatch(setNotification({
-          type: "error",
-          message: "Wrong username or password",
-        })
-        )
+        dispatch(
+          setNotification({
+            type: "error",
+            message: "Wrong username or password",
+          })
+        );
       } else {
-        dispatch(setUser(user))
-        blogService.setToken(user.token)
-        window.localStorage.setItem("user", JSON.stringify(user))
-        navigate('/')
+        dispatch(setUser(user));
+        blogService.setToken(user.token);
+        window.localStorage.setItem("user", JSON.stringify(user));
+        navigate("/");
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+    setUsername('')
+    setPassword('')
+  };
 
   return (
     <>
       <Notification />
-      <form onSubmit={handleLogin}>
-        <Header text="Login" />
-        <div className="">
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={({ target }) => setUsername(target.value)}
-            required
-          />
+      <section className="grid h-screen lg:-mt-20 sm:place-content-center sm:-mt-[20%] md:-mt-[10%]">
+        <div className="w-full max-w-sm  sm:w-[700px] p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+              Sign in to Blogs
+            </h5>
+            <div>
+              <label
+                for="username"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your username
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                placeholder="aroyehun"
+                value={username}
+                onChange={({ target }) => {
+                  console.log(target.value)
+                  setUsername(target.value)
+                  console.log(username)
+                }
+                }
+                required
+              />
+            </div>
+            <div>
+              <label
+                for="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••"
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                required
+              />
+            </div>
+            <div className="flex items-start">
+              <div className="flex items-start">
+                <div className="flex items-center h-5">
+                  <input
+                    id="remember"
+                    type="checkbox"
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                    required
+                  />
+                </div>
+                <label
+                  for="remember"
+                  className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Remember me
+                </label>
+              </div>
+              <a
+                href="#"
+                className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
+              >
+                Lost Password?
+              </a>
+            </div>
+            <button
+              type="submit"
+              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Login to your account
+            </button>
+            <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+              Not registered?{" "}
+              <Link
+                to='/signup'
+                className="text-blue-700 hover:underline dark:text-blue-500"
+              >
+                Create account
+              </Link>
+            </div>
+          </form>
         </div>
-        <div className="">
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
--- src/components/auth/SignupForm.jsx
+++ src/components/auth/SignupForm.jsx
@@ -27,6 +27,7 @@ const SignupForm = () => {
       console.log(user)
       dispatch(setUser(user))
 
+      console.log(user.token)
       blogService.setToken(user.token)
       window.localStorage.setItem("user", JSON.stringify(user))
       navigate('/')
@@ -39,33 +40,52 @@ const SignupForm = () => {
   }
 
   return (
-    <form onSubmit={addNewUser}>
+    <form onSubmit={addNewUser} className="mt-6 px-4 max-w-[540px]">
       <Header text="Sign up" />
-      <div className="">
-        Name:
+      <div className="relative w-full z-0 mb-6 group">
         <input
           type="text"
           value={name}
+          id="name"
           onChange={({ target }) => setName(target.value)}
+          className="signup-input peer"
+          required
         />
+        <label 
+          htmlFor="name"
+          className="label"
+        >Name</label>
       </div>
-      <div className="">
-        Username:
+      <div className="relative w-full z-0 mb-6 group">
         <input
           type="text"
           value={username}
+          id="username"
           onChange={({ target }) => setUsername(target.value)}
+          className="signup-input peer"
         />
+        <label 
+          htmlFor="username"
+          className="label"
+        >Username</label>
       </div>
-      <div className="">
-        Password:
+      <div className="relative w-full z-0 mb-6 group">
         <input
           type="password"
           value={password}
           onChange={({ target }) => setPassword(target.value)}
+          className="signup-input peer"
         />
+        <label 
+          htmlFor="password"
+          className="label"
+        >Password</label>
       </div>
-      <button type="submit">Signup</button>
+      <button 
+        type="submit" 
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
+        font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >Submit</button>
     </form>
   )
 }
